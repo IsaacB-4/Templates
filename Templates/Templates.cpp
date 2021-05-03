05b4c780c5b81137b8ee6815f33fa4e7b1af97e7
Isaac Booth <TheRealIkeB@gmail.com>
2021-05-02 20:48:27 -0400

    Created the driver for the half function

--- Templates/Templates.cpp
+++ Templates/Templates.cpp
@@ -1,3 +1,20 @@
 //Isaac Booth
 //CIS 1202 101
 //May 2, 2021
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Driver for half
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+	return 0;
+}
